3eb11512672e80bb7675f2dd606a0adc4ce28489
justinkirk8 <96632211+justinkirk8@users.noreply.github.com>
2022-05-11 15:45:04 -0400

    challenge 10 deliverable 2 update

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemi_pics(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,42 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemi_pics(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    html = browser.html
+    base = soup(html, 'html.parser')
+
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    hemi_collaps = base.find('div', class_= 'collapsible results')
+    hemi_items = hemi_collaps.find_all('div', class_='item')
+
+    for hemi in hemi_items:
+        # get image url 
+        hemi_pic_url = hemi.find('a')['href']
+        browser.visit(url + hemi_pic_url)
+        html_temp = browser.html
+        base_temp = soup(html_temp, 'html.parser') 
+        url_ul = base_temp.find('ul')
+        temp_href = url_ul.find_all('a')[0]['href']
+        temp_url = url + temp_href
+        
+        browser.visit(url)
+        # get title
+        temp_title = hemi.find('h3').text
+        
+        # add both as dictionary to list
+        hemisphere_image_urls.append({'img_url':temp_url,'title':temp_title})
+    
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
