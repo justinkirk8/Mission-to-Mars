2d27257d4529bd9b264c1ff135a4f01ed6c25ccb
justinkirk8 <96632211+justinkirk8@users.noreply.github.com>
2022-05-12 12:59:21 -0400

    challenge 10 deliverable 3 update

--- scraping.py
+++ scraping.py
@@ -96,7 +96,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-striped table-hover table-bordered")
 
 def hemi_pics(browser):
     # 1. Use browser to visit the URL 
